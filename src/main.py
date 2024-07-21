a04ba615606f44787fb636e712a6251c0840b6b7
Josué <josueavx@gmail.com>
2024-07-20 21:43:39 -0300

    feat: calling main methods

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from robocorp.tasks import task
 from .browser_manager import BrowserManager
 from .scraper import Scraper
+from .logger import Logger
 from time import sleep
+from src.utils import save_to_csv, download_images
 import traceback
 import json
 
@@ -15,14 +17,32 @@ def load_config(file_path):
 def search_and_store():
     try:
         config = load_config('config/config.json')
-
+        
+        logger_instance = Logger(__name__)
+        log = logger_instance.get_logger()
+        
         browser = BrowserManager()
-        scraper = Scraper(browser, config)
+        scraper = Scraper(browser, config, log)
 
         browser.start('https://www.latimes.com/')
+        
         scraper.search_news()
+        
+        scraper.sort_search('Newest')
+        sleep(2)
+        scraper.choose_topic()
+        sleep(2)
         scraper.collect_news()
+        
+        results = scraper.get_results()
+        
+        log.info('Downloading news')
+        download_images(results)
+        
+        log.info('Saving .csv file')
+        save_to_csv(results)
         
     except Exception:
         traceback.print_exc()
+        sleep(60)
         browser.close()
