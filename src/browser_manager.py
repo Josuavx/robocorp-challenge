e17bff6e138ca6610ce93d160df9740238dea464
Josué <josueavx@gmail.com>
2024-07-28 21:00:11 -0300

    feat: doc

--- src/browser_manager.py
+++ src/browser_manager.py
@@ -1,5 +1,5 @@
 from RPA.Browser.Selenium import Selenium
-from selenium.webdriver.chrome.options import Options
+
 
 class BrowserManager:
     def __init__(self) -> None:
@@ -8,7 +8,6 @@ class BrowserManager:
     def start(self, url: str) -> None:
         """Open a URL in the browser."""
         self.browser.open_chrome_browser(url, maximized=True, headless=True)
-        # self.browser.open_browser(url, browser='chrome')
 
     def close(self) -> None:
         """Close the browser."""
